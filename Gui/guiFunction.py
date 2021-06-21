395478cf752e690ae9fdeefe0f7d39c2a2c1319f
Ailson Araujo <ailson.a.leite@gmail.com>
2021-06-20 22:44:19 -0300

    implementado class nas funções

--- Gui/guiFunction.py
+++ Gui/guiFunction.py
@@ -4,107 +4,103 @@ from PySide6.QtWidgets import QGraphicsDropShadowEffect, QSizeGrip
 
 TIME_ANIMATION = 500
 MAXIMIZED = False
-SELF = None
 
 
-def setObject(self):
-    """Define um Objeto global para eventos onde não se
-    pode passar o objeto como parâmetro"""
+class Function:
+    """Trata a estilização da interface"""
 
-    global SELF
-    SELF = self
+    # Recebe o objeto pai
+    def __init__(self, parent: object):
+        self._parent = parent
 
+    def setDevelopVersion(self, develop: str, version: str):
+        """Define texto nas label de desenvolvedor e versão."""
 
-def setPosition(self, event):
-    """Recebe a posição do mouse usado para complemetar
-    a função que movimenta a janela"""
+        self._parent.labelDevelop.setText(develop)
+        self._parent.labelVersion.setText(version)
 
-    SELF.dragPos = event.globalPos()
+    def setPosition(self, event):
+        """Recebe a posição do mouse. Usado para complemetar
+        a função que movimenta a janela"""
 
+        self._parent.dragPos = event.globalPos()
 
-def setGrip(self):
-    """Define os Grip para redimensionamento da janela"""
+    def setGrip(self):
+        """Define os Grip para redimensionamento da janela"""
 
-    self.sizegrip = QSizeGrip(self.frGrip)
+        self._parent.sizegrip = QSizeGrip(self._parent.frGrip)
 
+    # Remove bordas e background do objeto
+    def removeCentralWidget(self):
+        self._parent.setWindowFlags(Qt.FramelessWindowHint)
+        self._parent.setAttribute(Qt.WA_TranslucentBackground)
 
-# Remove moldura e background do objeto
-def removeCentralWidget(self):
-    self.setWindowFlags(Qt.FramelessWindowHint)
-    self.setAttribute(Qt.WA_TranslucentBackground)
+    # Define uma sombra ao widget
+    def setDropShadow(self, widget: object):
+        self._parent.shadow = QGraphicsDropShadowEffect(self._parent)
+        self._parent.shadow.setBlurRadius(17)
+        self._parent.shadow.setXOffset(0)
+        self._parent.shadow.setYOffset(0)
+        self._parent.shadow.setColor(QColor(0, 0, 0, 150))
+        widget.setGraphicsEffect(self._parent.shadow)
 
+    # Maximiza e Restaura a janela
+    def maximizeRestore(self):
 
-# Define uma sombra ao widget
-def setDropShadow(self, widget):
-    self.shadow = QGraphicsDropShadowEffect(self)
-    self.shadow.setBlurRadius(17)
-    self.shadow.setXOffset(0)
-    self.shadow.setYOffset(0)
-    self.shadow.setColor(QColor(0, 0, 0, 150))
-    widget.setGraphicsEffect(self.shadow)
+        global MAXIMIZED
 
+        if MAXIMIZED == False:
+            self._parent.showMaximized()
+            MAXIMIZED = True
+            self._parent.gridLayout.setContentsMargins(0, 0, 0, 0)
+            self._parent.btMax.setToolTip("Restaurar")
+            self._parent.btMax.setIcon(QIcon(u":/16x16/16x16/cil-restore.png"))
+            self._parent.frGrip.hide()
 
-# Construção
-def maximizeRestore(self):
+        else:
+            MAXIMIZED = False
+            self._parent.showNormal()
+            self._parent.resize(self._parent.width() + 1, self._parent.height() + 1)
+            self._parent.gridLayout.setContentsMargins(10, 10, 10, 10)
+            self._parent.btMax.setToolTip("Maximizar")
+            self._parent.btMax.setIcon(QIcon(u":/16x16/16x16/cil-media-stop.png"))
+            self._parent.frGrip.show()
 
-    global MAXIMIZED
+    # Maximiza a janela
+    def dobleClickMaximizeRestore(self, event):
+        if event.type() == QEvent.MouseButtonDblClick:
+            QTimer.singleShot(250, lambda: self.maximizeRestore())
 
-    if MAXIMIZED == False:
-        self.showMaximized()
-        MAXIMIZED = True
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.btMax.setToolTip("Restaurar")
-        self.btMax.setIcon(QIcon(u":/16x16/16x16/cil-restore.png"))
-        self.frGrip.hide()
+    # Move a janela
+    def moveWindow(self, event):
 
-    else:
-        MAXIMIZED = False
-        self.showNormal()
-        self.resize(self.width() + 1, self.height() + 1)
-        self.gridLayout.setContentsMargins(10, 10, 10, 10)
-        self.btMax.setToolTip("Maximizar")
-        self.btMax.setIcon(QIcon(u":/16x16/16x16/cil-media-stop.png"))
-        self.frGrip.show()
+        # Restaura o tamanho da janela quando maximizada
+        if MAXIMIZED:
+            self.maximizeRestore()
 
+        if event.buttons() == Qt.LeftButton:
+            self._parent.move(self._parent.pos() + event.globalPos() - self._parent.dragPos)
+            self._parent.dragPos = event.globalPos()
+            event.accept()
 
-# Maximiza a janela
-def dobleClickMaximizeRestore(event):
-    if event.type() == QEvent.MouseButtonDblClick:
-        QTimer.singleShot(250, lambda: maximizeRestore(SELF))
+    # Alterna a largura do frame
+    def extendFrame(self, maxWidth: int, frame: object):
 
+        # obtem a largura
+        width = frame.width()
+        width_standard = 50
 
-# Move a janela
-def moveWindow(event):
+        # Define a maxima largura
+        if width == width_standard:
+            widthExtended = maxWidth
 
-    # Restaura o tamanho da janela quando
-    # quando maximizada
-    if MAXIMIZED:
-        maximizeRestore(SELF)
+        else:
+            widthExtended = width_standard
 
-    if event.buttons() == Qt.LeftButton:
-        SELF.move(SELF.pos() + event.globalPos() - SELF.dragPos)
-        SELF.dragPos = event.globalPos()
-        event.accept()
-
-
-# Alterna a largura do menu
-def extendMenu(self, maxWidth, frame):
-
-    # obtem a largura
-    width = frame.width()
-    width_standard = 50
-
-    # Define a maxima largura
-    if width == width_standard:
-        widthExtended = maxWidth
-
-    else:
-        widthExtended = width_standard
-
-    # Animação
-    self.animation = QPropertyAnimation(frame, b"minimumWidth")
-    self.animation.setDuration(TIME_ANIMATION)
-    self.animation.setStartValue(width)
-    self.animation.setEndValue(widthExtended)
-    self.animation.setEasingCurve(QEasingCurve.InOutQuart)
-    self.animation.start()
+        # Animação
+        self._parent.animation = QPropertyAnimation(frame, b"minimumWidth")
+        self._parent.animation.setDuration(TIME_ANIMATION)
+        self._parent.animation.setStartValue(width)
+        self._parent.animation.setEndValue(widthExtended)
+        self._parent.animation.setEasingCurve(QEasingCurve.InOutQuart)
+        self._parent.animation.start()
