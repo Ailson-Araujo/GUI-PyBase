155071439f8af4029eb3fa83db1eb089eb81eadc
Ailson Araujo <ailson.a.leite@gmail.com>
2021-06-26 12:08:33 -0300

    inseida funções para os button e modificado none de função

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+########################################################################
+## Copyright © 2021, Ailson Araujo
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## Versão: 1.0
+##
+## Projeto feito com Qt Designer, PySide6.
+## Baseado no projeto de WANDERSON M.PIMENTA
+########################################################################
+
 import os
 import sys
 
@@ -25,20 +46,28 @@ class WinPyBase(QMainWindow, Ui_PyBase):
         function.setDropShadow(self.frBase)
         function.removeCentralWidget()
         function.setDevelopVersion(__develop__, __version__)
-        function.setStyleTheme("themes/base_dark.qss")
+        function.setAppName("Sua Aplicação")
+        function.setStyleTheme("stylesheet/base_dark.qss")
+
+        self.stackedWidget.setCurrentIndex(0)
 
         self.frTitleBar.mouseDoubleClickEvent = function.dobleClickMaximizeRestore
         self.frTitleBar.mouseMoveEvent = function.moveWindow
+        self.setDesignerSelectedButton(function)
 
         self.btMin.clicked.connect(lambda: self.showMinimized())
         self.btMax.clicked.connect(lambda: function.maximizeRestore())
         self.btClose.clicked.connect(lambda: self.close())
 
         self.btHide.clicked.connect(lambda: function.extendFrame(190, self.frMenuLateral))
+        self.bt.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.bt2.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
 
-        self.bt.clicked.connect(lambda: function.setButtomSelect(self.bt))
-        self.bt2.clicked.connect(lambda: function.setButtomSelect(self.bt2))
-        self.bt3.clicked.connect(lambda: function.setButtomSelect(self.bt3))
+    def setDesignerSelectedButton(self, function: object):
+        self.bt.clicked.connect(lambda: function.setButtonSelected(self.bt))
+        self.bt2.clicked.connect(lambda: function.setButtonSelected(self.bt2))
+        self.bt3.clicked.connect(lambda: function.setButtonSelected(self.bt3))
+        self.btSettings.clicked.connect(lambda: function.setButtonSelected(self.btSettings))
 
     def mousePressEvent(self, event):
         function = Function(self)
